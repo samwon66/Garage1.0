d06c32efa9312ece10498ad1ce3fa9e93b48a2c0
Sammy Wong <sammywong528@outlook.com>
2023-10-02 16:31:37 +0200

    Create Garage

--- Garage.cs
+++ Garage.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Garage1._0.Vehicles;
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,9 +8,18 @@ using System.Threading.Tasks;
 
 namespace Garage1._0
 {
-    internal class Garage
+    internal class Garage<T> : IEnumerable<T> where T : Vehicle
     {
-        public string Name { get; set; }
         public int Capacity {  get; set; }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
