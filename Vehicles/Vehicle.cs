debdd2acc80449845e417bdabc9dabca13055cfd
Sammy Wong <sammywong528@outlook.com>
2023-10-03 10:54:29 +0200

    Add 2 methods to Vehicle

--- Vehicles/Vehicle.cs
+++ Vehicles/Vehicle.cs
@@ -17,12 +17,25 @@
             ParkingSlot = parkingSlot;
         }
 
+        /// <summary>
+        /// Get all the data of a vehicle
+        /// </summary>
+        /// <param name="vehicle"></param>
+        /// <returns>a string of the data of vehicle</returns>
         public string Data(Vehicle vehicle)
         {
             return string.Empty;
         }
 
-        public 
+        /// <summary>
+        /// Get the registration number of the vhicle
+        /// </summary>
+        /// <param name="vehicle"></param>
+        /// <returns>registration number</returns>
+        public string GetRegNr(Vehicle vehicle)
+        {
+            return string.Empty;
+        }
 
     }
 }
