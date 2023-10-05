f208bdb18c9da985b525c26490131e4accff363e
Sammy Wong <coolstonester@gmail.com>
2023-10-05 10:35:45 +0200

    Add  Capacity to Garage

--- Garage.cs
+++ Garage.cs
@@ -7,8 +7,9 @@ namespace Garage1._0
     {
         private T[]? vehicles = null;
         
+        public int Capacity { get { return vehicles.Length; } }
         public int ParkingSlot { get; set; }
-        public int CountVehicle { get; set; }
+        public int CountVehicle { get; private set; } = 0;
 
         public Garage(int parkingSlot) => ParkingSlot = parkingSlot;
         public IEnumerator<T> GetEnumerator()
